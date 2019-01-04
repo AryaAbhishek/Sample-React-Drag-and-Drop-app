1f2954f29e7261312d6e3e0ebcf78b4023f81dbf
AryaAbhishek <abhishekarya001@yahoo.in>
2019-01-04 14:17:50 -0500

    push to git

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  state = {        
+    tasks: [
+            {name:"Learn React",category:"todo",bgcolor: "yellow"},  
+            {name:"Learn BackBone", category:"todo",bgcolor:"green"},  
+            {name:"Learn JavaScript",category:"inprocess",bgcolor:"red"},
+            {name:"javascript-app",category:"todo",bgcolor: "yellow"},  
+            {name:"BackBone-app", category:"inprocess",bgcolor:"green"},  
+            {name:"react-app",category:"complete",bgcolor:"red"}          
+      ]}
+      onDragStart = (ev, id)=>{
+        console.log("dragstart:",id);
+        ev.dataTransfer.setData("id",id);
+      }
+      onDragOver = (ev) =>{
+        ev.preventDefault();
+      }
+      onDrop = (ev, cat) => {       
+        let id = ev.dataTransfer.getData("id");
+        let tasks = this.state.tasks.filter((task) => {
+            if (task.name == id) {
+                     task.category = cat;           
+            }              
+             return task;       
+         });        
+         this.setState({           
+            ...this.state,           
+            tasks       
+         }); } 
+      render() {        
+        var tasks = { todo: [], 
+                      inprocess: [],
+                      complete: []       
+        }         
+        this.state.tasks.forEach ((t) => {               
+          tasks[t.category].push(<div 
+            key={t.name}                     
+            onDragStart={(e)=>this.onDragStart(e, t.name)}                    
+            draggable                    
+            className="draggable"                    
+            style={{backgroundColor: t.bgcolor}}>                       
+               {t.name}                
+          </div>);        
+        });
+        return (<div className="container-drag">
+          <h2 className="header">DRAG & DROP DEMO</h2>                  
+          <div className="todo"
+            onDragOver={(e)=>this.onDragOver(e)}                   
+            onDrop={(e)=>{this.onDrop(e, "todo")}}>                    
+            <span className="task-header">ToDo</span>                    
+            {tasks.todo}                
+          </div>
+          <div className="inprocess"
+            onDragOver={(e)=>this.onDragOver(e)}                    
+            onDrop={(e)=>{this.onDrop(e, "inprocess")}}>                    
+            <span className="task-header">InProcess</span>                    
+            {tasks.inprocess}                
+          </div>                
+          <div className="complete"
+            onDragOver={(e)=>this.onDragOver(e)}                    
+            onDrop={(e)=>this.onDrop(e, "complete")}>                     
+            <span className="task-header">COMPLETED</span>                     
+            {tasks.complete}                
+          </div>              
+      </div>);
+      }
 }
 
 export default App;
